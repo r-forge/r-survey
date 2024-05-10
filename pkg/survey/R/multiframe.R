06359fc963b06789201341079c180e3df88f9cfc
tlumley (Thomas Lumley) <thomas.lumley@r-project.org>
2024-05-10 02:36:09 +0000

    more multiframe
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-survey@351 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 762a7fe36fc2e3f0e93de294c4ef175c20e14eff

--- pkg/survey/R/multiframe.R
+++ pkg/survey/R/multiframe.R
@@ -134,6 +134,56 @@ model.frame.multiframe<-function(formula,...){
     do.call(rbind,common)              
 }
 
+update.multiframe<-function(design, ...){
+	ndesigns<-length(design$designs)
+	for(i in 1:ndesigns){
+		design$designs[[i]]<-update(design$designs[[i]],...)
+	}
+	design$call<-sys.call(-1)
+	design
+}
+
+dimnames.multiframe<-function(x,...){
+    maybe_names<-lapply(x$designs,colnames)
+    list(NULL,Reduce("intersect", maybe_names))
+    }
+
+
+
+"[.multiframe"<-function (x, i, ..., drop = TRUE) {
+    if (!missing(i)) {
+            if (is.logical(i)) 
+                x$design_weights[!i] <- 0
+            else if (is.numeric(i) && length(i)) 
+                x$design_weights[-i] <- 0
+            else {
+                tmp <- x$design_weights[i, ]
+                x$design_weights <- rep(0, length(x$design_weights))
+                x$design_weights[i, ] <- tmp
+            }
+        } 
+    if (...length()>0 && !is.null(x$variables)) {
+        for (d in length(x$designs)){
+            x[[d]]$variables <- x[[d]]$variables[, ..1, drop = FALSE]
+        }
+    }
+    x
+}
+
+subset.multiframe<-function (x, subset, ...) 
+{
+    e <- substitute(subset)
+    pf<-parent.frame()
+    rs <- lapply(x$designs, function(d) eval(e, d$variables, pf))
+    r<-do.call(c,rs)
+    r <- r & !is.na(r)
+    x <- x[r, ]
+    x$call <- sys.call(-1)
+    x
+}
+
+### Inference
+
 svytotal.multiframe<-function(x,design, na.rm=FALSE,...){
     if (inherits(x,"formula"))
         x<-multiframe_getdata(x, design$designs)
@@ -251,44 +301,6 @@ multiframevar<-function(x, dchecks){
 }
 
 
-dimnames.multiframe<-function(x,...){
-    maybe_names<-lapply(x$designs,colnames)
-    list(NULL,Reduce("intersect", maybe_names))
-    }
-
-
-
-"[.multiframe"<-function (x, i, ..., drop = TRUE) {
-    if (!missing(i)) {
-            if (is.logical(i)) 
-                x$design_weights[!i] <- 0
-            else if (is.numeric(i) && length(i)) 
-                x$design_weights[-i] <- 0
-            else {
-                tmp <- x$design_weights[i, ]
-                x$design_weights <- rep(0, length(x$design_weights))
-                x$design_weights[i, ] <- tmp
-            }
-        } 
-    if (...length()>0 && !is.null(x$variables)) {
-        for (d in length(x$designs)){
-            x[[d]]$variables <- x[[d]]$variables[, ..1, drop = FALSE]
-        }
-    }
-    x
-}
-
-subset.multiframe<-function (x, subset, ...) 
-{
-    e <- substitute(subset)
-    pf<-parent.frame()
-    rs <- lapply(x$designs, function(d) eval(e, d$variables, pf))
-    r<-do.call(c,rs)
-    r <- r & !is.na(r)
-    x <- x[r, ]
-    x$call <- sys.call(-1)
-    x
-}
 
 
 svyvar.multiframe<-function(x,design, na.rm=FALSE, ...) {
@@ -321,121 +333,7 @@ svyvar.multiframe<-function(x,design, na.rm=FALSE, ...) {
     attr(vv, "statistic") <- "variance"
     class(vv) <- c("svyvar", "svystat")
     vv
-    }
-
-reweight<-function(design,...) UseMethod("reweight")
-
-reweight.dualframe<-function(design, targets=NULL, totals=NULL,
-                             estimator=c("constant","expected"), theta=NULL, ...) {
-    
-    estimator<-match.arg(estimator)
-    nframes<-length(design$designs)
-    overlaps<-design$overlaps
-    frame_weights<-vector("list",nframes)
-    
-    if (estimator=="expected"){
-        maxes<-sapply(overlaps, max, na.rm=TRUE)
-        mins<-sapply(overlaps, function(x) min(x[x>0], na.rm=TRUE))
-        if (max(mins)>=1) 
-            overlaps_are_weights<-TRUE
-        else if(min(maxes)>1)
-            stop("overlaps must be all >=1 or all <=1")
-        else
-            overlaps_are_weights<-FALSE
-        
-        for(f in 1:nframes){
-            overlaps[[f]][overlaps[[f]]==0]<-NA
-            if (overlaps_are_weights) overlaps[[f]]<-1/overlaps[[f]]
-            frame_weights[[f]]<-(1/rowSums(1/overlaps[[f]],na.rm=TRUE))/weights(design$designs[[f]],"sampling")
-        }
-        frame_weights<-do.call(c, frame_weights)
-        
-        design_weights<-do.call(c, lapply(design$designs, weights, type="sampling"))
-        
-        rval<-list(designs=design$designs,overlaps=design$overlaps, frame_scale=design$frame_scale,
-                   frame_weights=frame_weights, design_weights=design_weights,
-                   call=sys.call(), dchecks=design$dchecks, estimator=estimator)
-        class(rval)<-class(design)
-        return(rval)  ##done
-    }
-    if (estimator=="constant" && !is.null(theta)){
-        frame_scale<-c(theta, 1-theta)
-        for(f in 1:nframes){
-            frame_weights[[f]]<-ifelse(overlaps[[f]][,3-f]>0, frame_scale[f], 1)
-        }
-        frame_weights<-do.call(c, frame_weights)
-        design_weights<-do.call(c, lapply(design$designs, weights, type="sampling"))
-        
-        rval<-list(designs=design$designs,overlaps=design$overlaps, frame_scale=design$frame_scale,
-                   frame_weights=frame_weights,  design_weights=design_weights,
-                   call=sys.call(), dchecks=design$dchecks, estimator=estimator)
-        class(rval)<-class(design)
-        return(rval) ##done
-    }
-    
-    ## if we get here we are optimising something
-    if (!xor(is.null(targets), is.null(totals)))
-        stop("for estimator='constant', must provide exactly one of targets, totals, and theta")
-
-    if (!is.null(totals)){
-        targets<-lapply(totals,
-                        function(formula) bquote(vcov(svytotal(.(formula), design=.DESIGN,na.rm=TRUE))))
-        }
-    if(!is.null(design$theta))
-        theta_old<-design$theta
-    else
-        theta_old<-1/length(design$designs)
-    
-    theta_grid<-seq(0,1,by=0.05)
-    ntargets<-length(targets)
-    nthetas<-length(theta_grid)
-    variances<-lapply(1:ntargets, function(x) numeric(nthetas))   
-    for (j in 1:nthetas){
-        frame_scale<-c(theta_grid[j], 1-theta_grid[j])
-        for(i in 1:ntargets){
-            frame_weights<-vector("list",nframes)
-            for(f in 1:nframes){
-                frame_weights[[f]]<-ifelse(overlaps[[f]][,3-f]>0, frame_scale[f], 1)
-            }
-            frame_weights<-do.call(c, frame_weights)
-            design_weights<-do.call(c, lapply(design$designs, weights, type="sampling"))
-            
-            tempval<-list(designs=design$designs,overlaps=overlaps, frame_scale=design$frame_scale,
-                          frame_weights=frame_weights,  design_weights=design_weights,
-                          call=sys.call(), dchecks=design$dchecks, estimator=estimator)
-            class(tempval)<-class(design)
-            target<-do.call(substitute, list(targets[[i]], list(.DESIGN=tempval)))
-            estimator<-eval(target)
-            if (length(estimator)>1) {
-                warning(paste("multiple variances reported, only the first used for",deparse(targets[[i]])))
-                estimator<-estimator[1]
-                }
-            variances[[i]][j]<-estimator
-        }
-    }
-    reweight_info<-list(theta=theta_grid, targets=targets, variances=variances,
-                        theta_old=theta_old, opt_thetas=theta_grid[sapply(variances, which.min)])
-    class(reweight_info)<-"reweight_info"
-    rval<-design
-    rval$rewt<-reweight_info
-    class(rval)<-c("dualframe_with_rewt", class(design))
-    rval
 }
 
-coef.dualframe_with_rewt<-function(object, ...) object$rewt$opt_thetas
 
-plot.dualframe_with_rewt<-function(x,y,type="b",...){
-    ntargets<-length(x$rewt$targets)
-    scaled_vars<-vector("list",ntargets)
-    for(i in 1:ntargets){
-        fn<-approxfun(x$rewt$theta, x$rewt$variances[[i]])
-        old_var<-fn(x$rewt$theta_old)
-        scaled_vars[[i]]<-x$rewt$variances[[i]]/old_var
-    }
-    matplot(x$rewt$theta, do.call(cbind,scaled_vars),type=type,xlab="theta",ylab="Scaled variance",...)
-    
-    invisible(list(x$rewt$theta,scaled_vars))
-}
-
-reweight.multiframe<-function(design, ...) stop("multi-frame reweighting under construction")
-
+svychisq.multiframe<-svychisq.survey.design
