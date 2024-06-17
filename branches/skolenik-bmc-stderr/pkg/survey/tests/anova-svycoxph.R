e823b9c43c1a23fc4bff060090aef3c860121afd
skolenik (Stas  Kolenikov) <skolenik@gmail.com>
2024-06-17 16:24:57 +0000

    branch not needed anymore
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-survey@366 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: e1a0257aa4a0052d51a4cfde8e790dd94658ee53

--- branches/skolenik-bmc-stderr/pkg/survey/tests/anova-svycoxph.R
+++
@@ -1,7 +0,0 @@
-
- library(survey)
-example(svycoxph, ask=FALSE)
-m<-update(model, .~.+I(protime^2))
-a<-anova(m,model)
-b<-anova(m, model,force=TRUE)
-stopifnot(isTRUE(all.equal(b[2:6],a[c(3,4,6,7,8)])))
