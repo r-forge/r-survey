8eb7783316f319bd75201aa543abfd6cafb09e89
skolenik (Stas  Kolenikov) <skolenik@gmail.com>
2024-06-20 14:19:52 +0000

    Bell-McAffrey standard errors and degf documented; degf propagated to confint.svyglm(); some tests added; dev version bumped
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-survey@368 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 66b524ef0838ca4c8735cc64c3072e78e72c061c

--- branches/dev-stask-bmc-stderr/pkg/survey/R/survey.R
+++ branches/dev-stask-bmc-stderr/pkg/survey/R/survey.R
@@ -1093,7 +1093,7 @@ svyglm<-function(formula, design,subset=NULL,family=stats::gaussian(),start=NULL
 
 svyglm.survey.design<-function(formula,design,subset=NULL, family=stats::gaussian(),start=NULL,
                                rescale=TRUE, deff=FALSE, influence=FALSE,
-                               std.errors=c("linearized","Bell-McAffrey","Bell-McAffrey-2"),
+                               std.errors=c('linearized','Bell-McAffrey','Bell-McAffrey-2'),
                                degf=FALSE,
                                ...){
   
@@ -1163,11 +1163,15 @@ svyglm.survey.design<-function(formula,design,subset=NULL, family=stats::gaussia
     
     if (!is.null(getOption("svy.debug.bmca"))) browser()
     
-    if (is.null(attr(g$cov.unscaled, "degf.bmca")))
+    if (is.null(attr(g$cov.unscaled, "degf.bmca"))) 
       g$df.residual <- degf(design)+1-length(coef(g)[!is.na(coef(g))])
-    else
+    else {
       g$df.residual <- min(attr(g$cov.unscaled, "degf.bmca"))
-    
+      # this is supposed to be a vector
+      g$df.coef <- attr(g$cov.unscaled, "degf.bmca")
+      names(g$df.coef) <- names(coef(g))
+    }
+      
     # should g$df.null be reset to degf(design) ???
     
     class(g)<-c("svyglm",class(g))
@@ -1222,168 +1226,6 @@ vcov.svyglm<-function(object,...) {
   dimnames(v)<-list(names(coef(object)),names(coef(object)))
   v
 }
-
-#' @param resid residuals (vector) or estimating functions (matrix)
-#' @param design the survey design, trimmed to missing data and subset conditions
-#' @param regressors design matrix of the glm predictors from model.matrix()
-#' @param std.errors "Bell-McAffrey" for Bell-McAffrey standard errors 
-#' with unit working covariance matrix; "
-#' Bell-McAffrey-2" for Bell-McAffrey standard errors 
-#' with exchangeable correlation working covariance matrix
-#' @returns the vector of scaled residuals
-scale_bell_mcaffrey<-function(resid,design,regressors,std.errors, degf=FALSE) {
-  
-  if (!is.null(getOption("svy.debug.bmca"))) browser()
-  
-  resid2<-as.matrix(resid)
-  
-  if (nrow(resid2)!=nrow(design$variables) | nrow(resid2)!=nrow(regressors))
-    stop("length mismatch: this should not be happening.")
-
-  XtX<-crossprod(regressors)
-  invXtX<-chol2inv(chol(XtX))
-  
-  if (degf) {
-    # nrow(data) x nrow(data) matrix -- should it be sparse??
-    H <- tcrossprod(tcrossprod(regressors, invXtX), regressors)
-    H <- diag(nrow(design$variables))-H
-    G <- list()
-  }
-  
-  for (k in 1:length(unique(design$cluster[,1]))) {
-    # pick up the cluster ID
-    clID<-unique(design$cluster)[k,1]
-    # pick up the row indices
-    cl_indices<-which(design$cluster[,1]==clID)
-
-    # if (length(cl_indices)>ncol(regressors)) {
-      # slice the regressor matrix; do not let R convert it to a vector!
-      this_X<-regressors[cl_indices,,drop=FALSE]
-      # slice the residual matrix
-      this_res<-resid2[cl_indices,]
-      # form the projection
-      this_proj<-tryCatch( tcrossprod( tcrossprod(this_X, invXtX), this_X), 
-        error = function(e) {
-          cat("Loop index = ", k, "\n")
-          cat("Cluster ID = ",clID, "\n")
-          cat("Cluster indices = ", cl_indices, "\n")
-          cat("Regressor matrix dimensions are: \n", dim(this_X), "\n")
-          cat("(X'X)^{-1} matrix dimenstions are:\n", dim(invXtX), "\n")
-          cat("Residual vector is:\n", this_res, "\n")
-          # return zero matrix if the projection fails
-          matrix(rep(0,length(cl_indices)*length(cl_indices)), nrow=length(cl_indices))
-        }
-        
-      )
-      # update the residuals
-      A_i <- mihalf(diag(length(cl_indices))-this_proj)
-      resid2[cl_indices,]<-crossprod(A_i, this_res)
-      
-      # for the degrees of freedom calculation
-      if (degf) {
-        # Theorem 4 of Bell-McAffrey (SMJ 2002);
-        # this is the transpose of g_i
-        # the dimension is {nrow(design$variables)} x {# of regressors} 
-        # dfs are specific to parameters and have to be extracted by component
-        G[[k]] <- tcrossprod(crossprod(H[cl_indices,,drop=FALSE], A_i), tcrossprod( invXtX, this_X ))
-      }
-    # } endif dim regressors
-  }
-  
-  if (degf) {
-    if (std.errors=="Bell-McAffrey-2") {
-      # exchangeable correlation matrix, form an estimate
-      sigma2 <- sum(resid2*resid2)/length(resid2)
-      sigma_cross <- 0
-      n_cross <- 0
-      for (k in 1:length(unique(design$cluster[,1]))) {
-        # pick up the cluster ID
-        clID<-unique(design$cluster)[k,1]
-        # pick up the row indices
-        cl_indices<-which(design$cluster[,1]==clID)
-        
-        sigma_cross <- sigma_cross + sum(tcrossprod(resid2[cl_indices]))
-        n_cross <- n_cross + length(cl_indices)*length(cl_indices)
-      }
-      sigma_cross <- (sigma_cross - sum(resid2*resid2)) / (n_cross - length(resid2))
-      
-      # form monstrous V; should be a sparse matrix
-      V <- diag(sigma2,nrow=nrow(design$variables))
-      # replace off-diagonal elements within the same cluster
-      for (k in 1:length(unique(design$cluster[,1]))) {
-        # pick up the cluster ID
-        clID<-unique(design$cluster)[k,1]
-        # pick up the row indices
-        cl_indices<-which(design$cluster[,1]==clID)
-        V[cl_indices, cl_indices] <- sigma_cross
-      }
-      # the diagonal entries got overwritten
-      diag(V) <- rep(sigma2, nrow(V))
-    }
-    
-    lambda <- numeric()
-    for (j in 1:ncol(regressors)) {
-      # extract the j-th component from each G[[k]] 
-      this_G <- sapply(G, function(X, col) X[,col], j)
-      # this is supposed to be nrow(design$variables) x {# clusters} matrix
-      if (std.errors=="Bell-McAffrey") {
-        # identify matrix, simple!!!
-        GVG <- tcrossprod(this_G)
-      } else if (std.errors=="Bell-McAffrey-2") {
-        # exchangeable correlation matrix
-        GVG <- crossprod(crossprod(V, this_G), this_G)
-      } else {
-        stop("Standard errors type not supported: ", std.errors)
-      }
-      eigen_GVG <- eigen(GVG, symmetric=TRUE, only.values=TRUE)
-      this_df <- { sum(eigen_GVG$values)*sum(eigen_GVG$values) / 
-        sum(eigen_GVG$values*eigen_GVG$values) }
-      lambda[j]<-this_df
-    }
-    attr(resid2,"degf") <- lambda
-  }
-  
-  return(resid2)
-}
-
-#' Symmetric square root of a matrix
-#' 
-#' Computes the symmetric square root of a positive definite matrix
-#' 
-#' 
-#' @usage mhalf(M)
-#' @param M a positive definite matrix
-#' @return a matrix \code{H} such that \code{H^2} equals \code{M}
-#' @author Peter Hoff
-#' @export 
-mhalf <-
-  function(M) 
-  { 
-    #symmetric square  root of a pos def matrix
-    tmp<-eigen(M)
-    tmp$vec%*%sqrt(diag(tmp$val,nrow=nrow(M)))%*%t(tmp$vec)
-  }
-
-#' Symmetric inverse square root of a matrix
-#' 
-#' Computes the inverse symmetric square root of a positive definite matrix
-#' 
-#' 
-#' @usage mihalf(M)
-#' @param M a positive definite matrix
-#' @return a matrix $H$ such that $H^2$ equals $M^{-1]$
-#' @author Peter Hoff, Stas Kolenikov
-#' @export 
-mihalf <-
-  function(M) 
-  { 
-    #symmetric inverse square root of a pos def matrix
-    tmp<-eigen(M)
-    eigen0<-1/tmp$val
-    eigen0[tmp$val==0]<-0
-    tmp$vec%*%sqrt(diag(eigen0,nrow=nrow(M)))%*%t(tmp$vec)
-  }
-
 
 
 svy.varcoef<-function(glm.object,design,
@@ -1518,48 +1360,70 @@ summary.svyglm<-function (object, correlation = FALSE, df.resid=NULL,...)
 
 confint.svyglm<-function(object,parm,level=0.95,method=c("Wald","likelihood"),ddf=NULL,...){
     method<-match.arg(method)
+    # move parsing parm and pnames upfront as it is needed for Bell-McAffrey's d.f.s
+    pnames <- names(coef(object))
+    if (missing(parm)) 
+      parm <- seq_along(pnames)
+    else if (is.character(parm))
+      parm <- match(parm, pnames, nomatch = 0)
     if(method=="Wald"){        
+      if (is.null(object$df.coef)) {
         if (is.null(ddf))
-            ddf <- object$df.residual
+          ddf <- object$df.residual
         if (ddf<=0) {
-            ci<-confint.default(object,parm=parm,level=.95,...)*NaN
+          ci<-confint.default(object,parm=parm,level=.95,...)*NaN
         } else {
+          tlevel <- 1 - 2*pnorm(qt((1 - level)/2, df = ddf))
+          ci<-confint.default(object,parm=parm,level=tlevel,...)
+        }
+      } else {
+          if (!missing(ddf)) {  
             tlevel <- 1 - 2*pnorm(qt((1 - level)/2, df = ddf))
             ci<-confint.default(object,parm=parm,level=tlevel,...)
-        }
-        a <- (1 - level)/2
-        a <- c(a, 1 - a)
-        pct <- format.perc(a, 3)
-        colnames(ci)<-pct
-        return(ci)
-    }
-  pnames <- names(coef(object))
-  if (missing(parm)) 
-    parm <- seq_along(pnames)
-  else if (is.character(parm))
-    parm <- match(parm, pnames, nomatch = 0)
-  lambda<-diag(object$cov.unscaled[parm,parm,drop=FALSE])/diag(object$naive.cov[parm,parm,drop=FALSE])
-  if(is.null(ddf)) ddf<-object$df.residual
-  if (ddf==Inf){
+          } else {
+            ddf <- object$df.coef[parm]
+            # placeholder
+            ci <- confint.default(object,parm=parm,level=.95,...)*NaN
+            for (i in 1:length(parm)) {
+              tlevel <- 1 - 2*pnorm(qt((1 - level)/2, df = ddf[i]))
+              ci[i,]<-confint.default(object,parm=parm[i],level=tlevel,...)
+            }
+            attr(ci,"degf")<-ddf
+          }
+      }
+      a <- (1 - level)/2
+      a <- c(a, 1 - a)
+      pct <- format.perc(a, 3)
+      colnames(ci)<-pct
+      return(ci)
+  } else if(method=="likelihood") {
+    lambda<-diag(object$cov.unscaled[parm,parm,drop=FALSE])/diag(object$naive.cov[parm,parm,drop=FALSE])
+    if(is.null(ddf)) ddf<-object$df.residual
+    if (ddf==Inf){
       alpha<-pnorm(qnorm((1-level)/2)*sqrt(lambda))/2
-  }  else if (ddf<=0) {
-      stop("zero or negative denomintor df")
-  } else {
+    }  else if (ddf<=0) {
+      stop("zero or negative denominator df")
+    } else {
+      if (!is.null(object$df.coef)) ddf<-object$df.coef[parm]
       alpha<-pnorm(qt((1-level)/2,df=ddf)*sqrt(lambda))/2
+    }
+    rval<-vector("list",length(parm))
+    for(i in 1:length(parm)){
+      temp<-MASSprofile_glm(fitted=object,which=parm[i],alpha=alpha[i],...)
+      rval[[i]]<-confint_profile(temp,parm=parm[i],level=level, unscaled_level=2*alpha[i],...)
+    }
+    
+    names(rval)<-pnames[parm]
+    if (length(rval)==1)
+      rval<-rval[[1]]
+    else
+      rval<-do.call(rbind,rval)
+    attr(rval,"levels")<-level
+    if (!is.null(object$df.coef)) attr(rval,"degf")<-ddf
+    return(rval)
+  } else {
+    stop("Unknown confint.svyglm() method")
   }
-  rval<-vector("list",length(parm))
-  for(i in 1:length(parm)){
-    temp<-MASSprofile_glm(fitted=object,which=parm[i],alpha=alpha[i],...)
-    rval[[i]]<-confint_profile(temp,parm=parm[i],level=level, unscaled_level=2*alpha[i],...)
-  }
-  
-  names(rval)<-pnames[parm]
-  if (length(rval)==1)
-    rval<-rval[[1]]
-  else
-    rval<-do.call(rbind,rval)
-  attr(rval,"levels")<-level
-  rval
 }
 
 
