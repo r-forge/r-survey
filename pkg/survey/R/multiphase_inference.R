c38fe4d403a4c30fc18c081185075ee9c1ab6ba1
tlumley (Thomas Lumley) <thomas.lumley@r-project.org>
2024-07-01 00:35:50 +0000

    multiphase calibration (not quite right)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-survey@376 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: c1fa510c94c194f693293d0f1f6f6d6dbcce32df

--- pkg/survey/R/multiphase_inference.R
+++ pkg/survey/R/multiphase_inference.R
@@ -1,8 +1,8 @@
 
-multiphase<-function(ids, subset, strata, probs, data, fpc=NULL){
+multiphase<-function(ids, subset, strata, probs, data, fpc=NULL, check.variable.phase=TRUE){
     data<-detibble(data)
 
-    phase1_no_weights<-if(probs[[1]]==~1) TRUE else FALSE
+    phase1_no_weights<-identical(deparse(probs[[1]]), "~1") 
     
     subsets<-lookup_subset(subset, data)
     ids<-lookup_ids(ids, subsets, data)
@@ -21,8 +21,7 @@ multiphase<-function(ids, subset, strata, probs, data, fpc=NULL){
       has.fpc<-TRUE
     }
 
-
-    id_strata<- mapply(check_multistage, ids, strata, SIMPLIFY=FALSE) ##FIXME need to get these out again?
+    id_strata<- mapply(check_multistage, ids, strata, SIMPLIFY=FALSE) 
     
     ## make weighted covariance matrices for any stratified multistage phases
     
@@ -35,13 +34,15 @@ multiphase<-function(ids, subset, strata, probs, data, fpc=NULL){
         if (is.null(probs[[i]]))
             probs[[i]]<-compute_probs(str_i, id_i, this_subset)$allprob
         if (i==nphases)
-            htmats[[i]]<-Dcheck_multi(id_i[this_subset,,drop=FALSE], str_i[this_subset,,drop=FALSE], probs[[i]])
-        else 
+            htmats[[i]]<-Dcheck_multi(id_i[this_subset,,drop=FALSE],
+                                      str_i[this_subset,,drop=FALSE], probs[[i]])
+        else if (i==1 && phase1_no_weights){
+            n1<-sum(this_subset)
+            htmats[[i]]<-Diagonal(n1)
+        } else
             htmats[[i]]<-Dcheck_multi_subset(id_i,str_i, this_subset, probs[[i]], withrep)
 
     }
-    ## might want to keep htmats[[i]] the same size as subsets$cumulative[[i]] 
-    ## and do the final subsetting here so that rephase() is easier.
 
     htmats_sub<-vector("list", nphases)
     final_subset<-subsets$cumulative[[nphases]]
@@ -56,14 +57,13 @@ multiphase<-function(ids, subset, strata, probs, data, fpc=NULL){
         prefinal_subset<-subsets$cumulative[[nphases]][subsets$cumulative[[i-1]]]
         wi[[i]]<-1/(1-diag(htmats[[i]]))
         w[[i]]<-w[[i-1]][prefinal_subset]*wi[[i]]
-       
     }
     pdag<-vector("list",nphases)
     pdag[[nphases]]<-tcrossprod(1/wi[[nphases]])/(1-htmats_sub[[nphases]])
     for(i in (nphases-1):1){
         final_subset<-subsets$cumulative[[nphases]][subsets$cumulative[[i]]]
         if(i==1 && phase1_no_weights){
-            ## special case sampling with replacement
+            ## special case simple random sampling with replacement at phase 1
             pdag[[i]]<- tcrossprod(1/wi[[i]][final_subset])*pdag[[i+1]]
           }  else{
               pdag[[i]]<- tcrossprod(1/wi[[i]][final_subset])/(1-htmats_sub[[i]])*pdag[[i+1]]
@@ -76,6 +76,7 @@ multiphase<-function(ids, subset, strata, probs, data, fpc=NULL){
     dcheck[[nphases]]<-htmats_sub[[nphases]]
     ## where does each variable first appear
     variable_phases<-phase_for_var(data, subsets)
+    if (check.variable.phase && all(variable_phases==1)) warning("all variables measured at all phases")
 
     ## subpopulations: NA for not sampled this phase, TRUE/FALSE
     subpop<-ifelse(do.call(cbind, subsets$cumulative), TRUE, NA)
@@ -95,10 +96,9 @@ multiphase<-function(ids, subset, strata, probs, data, fpc=NULL){
 
 
 subset.multiphase<-function(x,subset,...){
-    warning("not ready yet")
   e <- substitute(subset)
   r <- eval(e, x$data, parent.frame())
-  r <- r & !is.na(r) ## ideally want to handle structural NAs differently
+  r <- r & !is.na(r) 
   x<-x[r,]
   x$call<-sys.call(-1)
   x
@@ -106,7 +106,7 @@ subset.multiphase<-function(x,subset,...){
 
 project_ps<-function(xc, psvar){
     if (is.null(psvar)) return(xc)
-    qr.resid(psvar$qr,xc/psvar$g)*psvar$g
+    qr.resid(psvar$qr,xc/psvar$w)*psvar$w
 }
 
 multiphasevar<-function(x, weights, htmats, subsets, postStrata){
@@ -144,7 +144,7 @@ print.multiphase<-function(x,...){
 
 svytotal.multiphase<-function(x, design, na.rm=FALSE, ...){
     x<-multiphase_getdata(x, design,formula_only=FALSE)
-    if (na.rm) { ## FIXME
+    if (na.rm) { 
         nas <- rowSums(is.na(x))
         design <- design[nas == 0, ]
         x[nas > 0, ] <- 0
@@ -162,7 +162,7 @@ svytotal.multiphase<-function(x, design, na.rm=FALSE, ...){
 
 svymean.multiphase<-function(x, design, na.rm=FALSE, ...){
     x<-multiphase_getdata(x, design,formula_only=FALSE)
-    if (na.rm) { ## FIXME
+    if (na.rm) {
         nas <- rowSums(is.na(x))
         design <- design[nas == 0, ]
         x[nas > 0, ] <- 0
@@ -243,3 +243,123 @@ svyvar.multiphase<-function (x, design, na.rm = FALSE, ...)
     class(vv) <- c("svyvar", "svystat")
     vv
 }
+
+calibrate.multiphase<-function(design, formula, phase, to.phase=phase-1, population=NULL,
+                               bounds=c(-Inf,Inf), calfun=c("linear","raking","logit"),
+                               maxit=50, epsilon=1e-7, verbose=FALSE, force=FALSE, trim=NULL,
+                               bounds.const = FALSE, sparse=FALSE, ...){
+    if (phase-1!=to.phase)
+        stop("only calibration of single phases supported at this time")
+    if (phase==1 && is.null(population))
+        stop("need population for calibration of phase 1")
+    nphases<-design$nphases
+    if (phase>nphases || phase <1) stop(paste("there is no phase",phase))
+    if (!is.list(bounds)) 
+        bounds <- list(lower = bounds[1], upper = bounds[2])
+  
+    if (is.character(calfun)) calfun<-match.arg(calfun)
+    if (is.character(calfun) && calfun=="rrz"){
+        design<-estWeights(design, formula,phase=phase, to.phase=to.phase,...)
+        design$call<-sys.call(-1)
+        return(design)
+    }
+    if (is.character(calfun)) 
+        calfun <- switch(calfun, linear = cal.linear, raking = cal.raking, 
+                         logit = cal.logit)
+    if (!inherits(calfun, "calfun")) 
+        stop("'calfun' must be a string or of class 'calfun'.")
+    
+    if(!inherits(formula,"formula")){
+        if (inherits(formula, "glm") || inherits(formula, "coxph") || inherits(formula, "lm")){
+            if (is.character(calfun))
+                calfun<-switch(calfun,linear=cal.linear, raking=cal.raking, logit=cal.logit)
+            rval<-inf_calibrate(design, working_model=formula, add_mat=NULL,
+                                calfun=calfun,...) ## FIXME: need new method?
+            return(rval)
+        } else stop("formula must be a formula or working model")
+    }
+    if (missing(population) || is.null(population)){
+        ## calibrate to to.phase totals
+        data_to<-design$data[design$subsets$cumulative[[to.phase]],
+                             all.vars(formula), drop=FALSE]
+        population<-colSums(model.matrix(formula, model.frame(formula, data_to)))
+        rm(data_to)
+    }
+
+    data_from<-design$data[design$subsets$cumulative[[phase]],
+                             all.vars(formula), drop=FALSE]
+    if(sparse){
+        mm<-sparse.model.matrix(formula, model.frame(formula, data_from))
+    }else{
+        mm<-model.matrix(formula, model.frame(formula, data_from))
+    }
+  
+    ww<-weights(design,"phase")[[phase]]
+    
+    ## if bounds are set to a constant convert to multiplicative value and run as normal
+    if (bounds.const)
+        bounds<-lapply(bounds,function(x) ifelse(ww>0,x/ww,x)) 
+    
+    whalf<-sqrt(ww)
+    sample.total<-colSums(mm*ww)
+  
+    if(any(sample.total==0)){
+        ## drop columns where all sample and population are zero
+        zz<-(population==0) & (apply(mm,2,function(x) all(x==0)))
+        mm<-mm[,!zz]
+        population<-population[!zz]
+        sample.total<-sample.total[!zz]
+        if (length(epsilon)>1) epsilon <- epsilon[!zz]
+    }
+    if (length(sample.total)!=length(population)){
+        print(sample.total)
+        print(population)
+        stop("Population and sample totals are not the same length.")
+    }
+    if(!is.null(names(population))){
+        if (!all(names(sample.total) %in% names(population))){
+            warning("Sampling and population totals have different names.")
+            cat("Sample: "); print(names(sample.total))
+            cat("Popltn: "); print(names(population))
+        }
+        else if (!all(names(sample.total) == names(population))){
+            warning("Sample and population totals reordered to make names agree: check results.")
+            population <- population[match(names(sample.total), names(population))]
+        }
+    }
+  
+    tqr<-qr(mm*whalf)
+    g<-grake(mm,ww,calfun, bounds=bounds,population=population,
+             verbose=verbose,epsilon=epsilon,maxit=maxit)
+    
+    if(!is.null(trim)) {
+        gnew<-pmax(trim[1], pmin(g, trim[2]))
+        outside<-g<trim[1] | g>trim[2]
+        if (any(outside)){
+            trimmings<-(g-gnew)*ww
+            gnew[!outside]<-gnew[!outside]+sum(trimmings)/sum(ww[!outside])
+            g<-gnew
+            attr(g,"failed")<-NULL
+            message(paste(sum(outside),"weights were trimmed"))
+        }
+    }
+    if (!is.null(attr(g,"failed"))){
+        if (!force) stop("Calibration failed")
+    }
+
+    phaseweights<-weights(design,"phase")
+    for(this_phase in phase:nphases){
+        this_subset<-design$subsets$cumulative[[this_phase]][design$subsets$cumulative[[phase]]]
+        phaseweights[[this_phase]]<-phaseweights[[this_phase]]*g[this_subset]
+    }
+    design$phaseweights<-phaseweights
+    design$finalweights<-phaseweights[[nphases]]
+  
+    caldata <- list(qr=tqr, w=g*whalf, stage=0, index=NULL, g=g)
+    class(caldata) <- c("phase_calibration", "gen_raking")
+    design$postStrata[[phase]] <- c(design$postStrata[[phase]],caldata)
+    design$call<-sys.call(-1)
+    design
+}
+
+  
